cf1586f1dc87b3a40bc3526c5ff8ea56729102c2
unknown <webdev.faizan@gmail.com>
2023-06-11 01:18:13 +0530

    The same function that is used to open the music file is now used to open the software file

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ def open_music_file(file_path):
         print("File not found.")
 
 
+def open_app(software_path):
+    if os.path.exists(software_path):
+        if os.name == 'nt':
+            subprocess.run(['start', software_path], shell=True)
+        elif os.name == 'posix':
+            subprocess.run([software_path])
+        else:
+            print("Unsupported OS. Cannot open the software.")
+    else:
+        print("Software not found.")
+
 def say(text):
     speaker.Speak(text)
 
@@ -50,9 +61,14 @@ while 1:
         # musicPath = "O:/Eevergren music ever/New Best/Ragnarok/Svartalfheim _ God of War Ragnarök (Original Soundtrack) ft. Eivør (128 kbps).mp3"
         # musicPath = '/path/to/your/music/file.mp3'
         open_music_file(musicPath)
-        say(f"Opening music sir...")
+        say(f"Playing Svartlefiem sir...")
         flag = 0
         # os.system(f"open {musicPath}")
+    elif "hello" in s.lower():
+        appPath = "C:/Program Files (x86)/Everything/Everything.exe"
+        open_app(appPath)
+        say(f"Opening Everything sir...")
+        flag = 0
 
     for site in sites:
         if f"Open {site[0]}".lower() in s.lower():
