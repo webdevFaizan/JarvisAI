cb116e7142943aa724072d2452ca2dba02d55267
unknown <webdev.faizan@gmail.com>
2023-06-10 07:49:38 +0530

    Able to say the typed string from windows

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import win32com.client
+import os
 
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+
+def say(text):
+    speaker.Speak(text)
+    # os.system(f"say {text}")
+
 
 while 1:
     print("Enter")
     s = input()
-    speaker.Speak(s)
+    say(s)
