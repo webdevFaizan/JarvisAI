42500dac5fa409b1555fc5b08c3b485f63fbf162
unknown <webdev.faizan@gmail.com>
2023-06-11 01:03:29 +0530

    Able to give voice command to open music through the assistant

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import win32com.client
 import speech_recognition as sr
 import webbrowser
+import os
+import subprocess
 
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+
+def open_music_file(file_path):
+    if os.path.exists(file_path):
+        if os.name == 'nt':
+            subprocess.run(['start', file_path], shell=True)
+        elif os.name == 'posix':
+            subprocess.run(['xdg-open', file_path])
+        else:
+            print("Unsupported OS. Cannot open the file.")
+    else:
+        print("File not found.")
 
 
 def say(text):
@@ -21,7 +35,7 @@ def takeCommand():
             return query
         except Exception as e:
             print("Some error occurred, Sorry")
-            return "Could not understand you"
+            return "Could not understand you, sorry"
 
 
 say("I am Jarvis A I")
@@ -30,6 +44,16 @@ while 1:
     # s = input()
     s = takeCommand()
     sites = [["youtube", "https://www.youtube.com"], ["twitter", "https://www.twitter.com"], ["instagram", "https://www.instagram.com"], ["facebook", "https://www.facebook.com"]]
+
+    if "play music" in s.lower():
+        musicPath = "C:/Users/XORPIAN/Desktop/Svartalfheim.mp3"
+        # musicPath = "O:/Eevergren music ever/New Best/Ragnarok/Svartalfheim _ God of War Ragnarök (Original Soundtrack) ft. Eivør (128 kbps).mp3"
+        # musicPath = '/path/to/your/music/file.mp3'
+        open_music_file(musicPath)
+        say(f"Opening music sir...")
+        flag = 0
+        # os.system(f"open {musicPath}")
+
     for site in sites:
         if f"Open {site[0]}".lower() in s.lower():
             say(f"Opening {site[0]} sir...")
@@ -37,3 +61,4 @@ while 1:
             flag = 0
     if flag == 1:
         say(s)
+
