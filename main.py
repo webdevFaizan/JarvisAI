168f74a56e88e80c010cb73eb58ccfa61004c908
unknown <webdev.faizan@gmail.com>
2023-06-10 06:59:14 +0530

    Created a code for inputing the String and outputting the voice message

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import win32com.client
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while 1:
+    print("Enter")
+    s = input()
+    speaker.Speak(s)
