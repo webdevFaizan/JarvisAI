1052755a8fcc972f41130038dcb8bfeb728482c7
unknown <webdev.faizan@gmail.com>
2023-06-10 22:28:38 +0530

    Able to take input from mic

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import win32com.client
-import os
+import speech_recognition as sr
 
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
 
 
 def say(text):
     speaker.Speak(text)
-    # os.system(f"say {text}")
 
 
+def takeCommand():
+    print("Listening...")
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said : {query}")
+            return query
+        except Exception as e:
+            print("Some error occurred, Sorry")
+            return "Could not understand you"
+
+
+say("I am Jarvis A I")
 while 1:
-    print("Enter")
-    s = input()
+    # s = input()
+    s = takeCommand()
     say(s)
